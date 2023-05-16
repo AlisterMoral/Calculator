00dbec697b4a580f1a06770fc1f7e98aaa4d6b9b
Alister <Alister.moral@gmail.com>
2023-05-16 13:49:24 +0200

    created 3 variable and 1 function that take an operator and 2 number

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Calculator
 
-I started the calculator project by creating multiple function that add,subtract,multiply,divie then tested them in the console 
+I started the calculator project by creating multiple function that add,subtract,multiply,divie then tested them in the console.
+
+Created a calculator operation by creating 3 variable 1 for the firstnumber one for the operator and last one for the seconde number.
+
+Then created a new function called operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
--- calculator.js
+++ calculator.js
@@ -16,9 +16,29 @@ function divide (num1, num2){
       }
       return num1 / num2;
     }
-console.log(add(6, 6)); //should be 12
-console.log(subtract(10, 4)); //should be 6
-console.log(multiply(2, 6));  //should be 12
-console.log(divide(15, 3)); //should be 5  
-console.log(divide(10, 0)); // error cannot divide by zero   
-//works for now ! 
+
+let firstNumber = 3;
+let operator = '+';
+let secondNumber = 5;
+
+function operate (operator, num1, num2){
+    switch (operator){
+        case '+':
+         return add (num1, num2);
+        case '-':
+         return subtract(num1, num2);
+        case '*':
+         return multiply(num1, num2);
+        case '/':
+         return divide(num1, num2);
+    default:
+         return "Invalid operator";
+    }
+}
+console.log(operate('+', 6, 6));    // should be 12 
+console.log(operate('-', 10, 4));   // should be 6
+console.log(operate('*', 2, 6));    // should be 12
+console.log(operate('/', 15, 3));   // should be  5
+console.log(operate('@', 10, 2));   //  Invalid operator
+
+//works for now !
