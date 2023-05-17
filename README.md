c2aa81cd7a027b9e5e33fac5c8efb471584c331e
Alister <Alister.moral@gmail.com>
2023-05-17 14:13:49 +0200

    making the calculator works!

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Created a calculator operation by creating 3 variable 1 for the firstnumber one 
 Then created a new function called operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
 Making basic HTML calculator  with button for each digits/operator/function an equal key 
+
+Creating function that populate the display when clicking the number button , storing the 'display value' in a variable.
+after this i make the calculator work by storing  number that is input into the calculator  when pressed  and save wich operation has been chosen and then 'operate()' on them when the user press "=" key.
+i already have the display so once 'operate()' has been called  it update the display with the solution  to the operation 
--- calculator.js
+++ calculator.js
@@ -17,9 +17,10 @@ function divide (num1, num2){
       return num1 / num2;
     }
 
-let firstNumber = 3;
-let operator = '+';
-let secondNumber = 5;
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+let displayValue = "";
 
 function operate (operator, num1, num2){
     switch (operator){
@@ -35,3 +36,75 @@ function operate (operator, num1, num2){
          return "Invalid operator";
     }
 }
+
+function updateDisplay(number) {
+    displayValue += number;
+    document.getElementById("display").textContent = displayValue;
+}
+  
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("display").textContent = "";
+}
+
+function handleNumberClick(number) {
+    updateDisplay(number);
+}
+
+  function handleOperatorClick(selectedOperator) {
+    if (operator === "" && firstNumber === "") {
+      firstNumber = displayValue;
+      operator = selectedOperator;
+      clearDisplay();
+    } else if (operator !== "" && firstNumber !== "") {
+      secondNumber = displayValue;
+      const result = operate(operator, Number(firstNumber), Number(secondNumber));
+      displayValue = String(result);
+      document.getElementById("display").textContent = displayValue;
+      firstNumber = displayValue;
+      operator = selectedOperator;
+      secondNumber = "";
+    }
+}
+  
+function handleEqualClick() {
+    if (operator !== "" && firstNumber !== "" && displayValue !== "") {
+      secondNumber = displayValue;
+      const result = operate(operator, Number(firstNumber), Number(secondNumber));
+      displayValue = String(result);
+      document.getElementById("display").textContent = displayValue;
+      firstNumber = "";
+      operator = "";
+      secondNumber = "";
+    }
+}
+
+//event listeners to digit
+const numberButtons = document.querySelectorAll('.digit');
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const number = button.textContent;
+    handleNumberClick(number);
+  });
+});
+
+// event listeners to operator
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const selectedOperator = button.textContent;
+    handleOperatorClick(selectedOperator);
+  });
+});
+
+// event listeners to clear button
+const clearButton = document.getElementById('clear-btn');
+clearButton.addEventListener('click', () => {
+  clearDisplay();
+});
+
+//event listeners to equals button
+const equalsButton = document.getElementById('equals-btn');
+equalsButton.addEventListener('click', () => {
+  handleEqualClick();
+});
